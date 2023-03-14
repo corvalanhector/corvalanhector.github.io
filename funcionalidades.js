160d399bd0ba7da5a4e9b503bfa3cf414c79e1b8
Hector Alfredo Corvalan <126353357+corvalanhector@users.noreply.github.com>
2023-03-14 13:27:03 -0300

    initial commit

---
+++ funcionalidades.js
@@ -0,0 +1,8 @@
+
+
+
+document.getElementById("boton").onclick = function () {
+    console.log("se realiza evento click");
+    document.getElementById("Mas_informacion").innerHTML = "Barry-Ruiz641@facebook.com";
+}
+   
